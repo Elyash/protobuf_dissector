d19784597c122977ac82e72d8b5d6f4be7738f15
Elyash <elyashiv12@gmail.com>
2020-03-19 11:38:14 +0200

    Added remote debug helper

---
+++ debug_lua.bat
@@ -0,0 +1,7 @@
+:: Put this file in your WireShark directory
+:: Run this file for remote debugging
+
+set ZBS=C:\Program Files\ZeroBraneStudio
+set LUA_PATH=.\?.lua;%ZBS%\lualibs/?/?.lua;%ZBS%\lualibs/?.lua
+set LUA_CPATH=%ZBS%\bin/?.dll;%ZBS%\bin/clibs52/?.dll
+tshark.exe
